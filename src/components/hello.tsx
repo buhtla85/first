a585c685eff0927c3751c5672b72a5af93a72fbe
buhtla85 <tamara.naunovic@gmail.com>
2019-08-26 15:46:52 +0200

    new functionalities

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -1,16 +1,46 @@
 import * as React from "react";
 
-export interface QuoteAuthor { 
-    quote: string; 
-    author: string;
-}
 
-export class TextComponent extends React.Component<QuoteAuthor, {}> {
-    render() {
+const quotesArr: Array<{quote: string, author: string}> = [
+    {quote: 'Nobody expects the Spanish Inquisition!', author: 'Michael Palin'},
+    {quote: "Spam! Spam! Spam! Spam! Spam! Spam!", author: "Terry Jones"},
+    {quote: "We're an anarcho-syndicalist commune.", author: "Michael Palin"},
+    {quote: "Venezuelan beaver cheese?", author: "John Cleese"}, 
+    {quote: "Your mother was a hamster and your father smelt of elderberries!", author: "John Cleese"},
+    {quote: "My hovercraft is full of eels.", author: "John Cleese"},
+    {quote: "He's not the Mesiah. He's a very naughty boy!", author: "Terry Jones"}
+  ];
+
+export interface IndexState {
+    index: number;
+}
+  
+
+export class TextComponent extends React.Component<{}, IndexState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            index: 0
+        }
+    }
+
+    public moveToNext = () => {
+        const randomInd: number = Math.floor(Math.random() * quotesArr.length);
+        this.setState({index: randomInd})
+    }
+    public render() {
+        const item = quotesArr[this.state.index]
         return (
-        <div>
-            <q>{this.props.quote}</q>
-            <section>{this.props.author}</section>
+        <div id="quote-box">
+            <h1>Monty Python Quotes</h1>
+            <blockquote>
+                <q>{item.quote}</q>
+                <footer>{item.author}</footer>
+            </blockquote>
+            <button>
+                <a id="tweet-quote" href={`https://twitter.com/intent/tweet?text=${item.quote} ${item.author}`} target="_blank" title="Post this quote on Twitter!">Tweet Quote</a>
+            </button>
+            <button onClick={this.moveToNext}>Next Quote</button>
         </div>
         );
     }
--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import { Bye } from "./components/bye";
 
 ReactDOM.render(
     <div>
-        <TextComponent quote="Random quote" author="Random Author" />
+        <TextComponent />
         <Bye compiler="C++" framework= "Angular" />
     </div>,
     document.getElementById("example")
