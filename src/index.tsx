5692ca199036916671d0caf6c83876366d62ffe8
buhtla85 <tamara.naunovic@gmail.com>
2019-09-13 12:41:17 +0200

    testing

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { TextComponent } from "./components/hello";
-import { Bye } from "./components/bye";
+import App from "./App";
 
-ReactDOM.render(
-    <div>
-        <TextComponent />
-        <Bye color="red" url="https://gist.githubusercontent.com/shreyasminocha/7d5dedafc1fe158f82563c1223855177/raw/325d51aca7165b2498971afcff9bed286a52dc0e/quotes.json" />
-        <Bye color="blue" url="https://gist.githubusercontent.com/shreyasminocha/7d5dedafc1fe158f82563c1223855177/raw/325d51aca7165b2498971afcff9bed286a52dc0e/quotes.json" />
-    </div>,
-    document.getElementById("root")
-);
+
+ReactDOM.render(<App />, document.getElementById("root"));
+    
