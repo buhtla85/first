20a12f80ef18d42ee16883ccf0b50435c2e70c23
buhtla85 <tamara.naunovic@gmail.com>
2019-10-10 18:14:31 +0200

    change

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 import App from "./App";
+//import {BrowserRouter as Router} from "react-router-dom";
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
