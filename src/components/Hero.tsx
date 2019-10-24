9795994eac07c6ed3839ffec5bb1bd85262d3052
buhtla85 <tamara.naunovic@gmail.com>
2019-10-24 23:50:48 +0200

    hero section component

---
+++ src/components/Hero.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Hero: React.SFC = () => {
+    return (
+        <div className="hero-image">
+            <div className="hero-text">
+                <h1>Welcome to the Furball Hotel</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi qui aperiam, adipisci nesciunt fugiat rem magnam voluptatum voluptatibus at pariatur!</p>
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
