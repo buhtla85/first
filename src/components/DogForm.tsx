fb663652001d00bcb161740005f2807164f545f2
buhtla85 <tamara.naunovic@gmail.com>
2019-11-06 19:08:25 +0100

    minor changes

--- src/components/DogForm.tsx
+++ src/components/DogForm.tsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import { differenceInDays } from "date-fns";
 
-interface IDogObject {
-    dogName: string,
-    dogBreed: string,
+interface IDog {
+    name: string,
+    breed: string,
     food: boolean,
     grooming: boolean,
     foodPrice: number,
@@ -10,38 +11,40 @@ interface IDogObject {
 }
 
 interface IStateDog {
-    dogs: IDogObject[]
+    dogs: IDog[]
 }
+
+//ovo je single dog - samo IDogObject
 
 export default class DogForm extends Component<{}, IStateDog> {
     constructor(props: any) {
         super(props);
         this.state = {
             dogs: [
-                {dogName: "", dogBreed: "", food: false, grooming: false, foodPrice: 0, groomPrice: 0}
+                {name: "", breed: "", food: false, grooming: false, foodPrice: 0, groomPrice: 0}
             ]
         }
     }
     
     addNewDog = () => {
         this.setState({
-            dogs: this.state.dogs.concat([{dogName:"", dogBreed: "", food: false, grooming: false, foodPrice: 0, groomPrice: 0}])
+            dogs: this.state.dogs.concat([{name:"", breed: "", food: false, grooming: false, foodPrice: 0, groomPrice: 0}])
         });
     }  
 
     removeDog = (index: number) => () => {
         this.setState({
-            dogs: this.state.dogs.filter((dog: IDogObject, dogIndex: number) => index !== dogIndex)
+            dogs: this.state.dogs.filter((dog: IDog, dogIndex: number) => index !== dogIndex)
         })
     } // if we store in the state two dog objects, but want to remove first, an error ocurs 
     // UPDATE: error fixed - just needed to bind value of the user input in the markup
 
     handleNameChange = (index: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
-        const newDog = this.state.dogs.map((dog: IDogObject, dogIndex: number) => {
-            if (index !== dogIndex) return dog;
-            return {...dog, dogName: event.target.value}
-        });
-        this.setState({dogs: newDog})
+        const newDog = this.state.dogs.map((dog: IDog, dogIndex: number) => 
+            index !== dogIndex ? dog : {...dog, name: event.target.value}
+        );
+        this.setState({dogs: newDog});
+
         // if ("form-control dogName".includes(event.target.className) || "form-control dogBreed".includes(event.target.className)) {
         //     let newDogs = [...this.state.dogs];
         //     newDogs[event.target.dataset.id][event.target.className] = event.target.value
@@ -55,15 +58,15 @@ export default class DogForm extends Component<{}, IStateDog> {
     }
 
     handleBreedChange = (index: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
-        const newDog = this.state.dogs.map((dog: IDogObject, dogIndex: number) => {
+        const newDog = this.state.dogs.map((dog: IDog, dogIndex: number) => {
             if (index !== dogIndex) return dog;
-            return {...dog, dogBreed: event.target.value}
+            return {...dog, breed: event.target.value}
         });
         this.setState({dogs: newDog})
     }
 
     handleFoodCheck = (index: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
-        const newDog = this.state.dogs.map((dog: IDogObject, dogIndex: number) => {
+        const newDog = this.state.dogs.map((dog: IDog, dogIndex: number) => {
             if (index !== dogIndex) return dog;
 
             if (event.target.checked === true) {
@@ -76,7 +79,7 @@ export default class DogForm extends Component<{}, IStateDog> {
     }
 
     handleGroomCheck = (index: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
-        const newDog = this.state.dogs.map((dog: IDogObject, dogIndex: number) => {
+        const newDog = this.state.dogs.map((dog: IDog, dogIndex: number) => {
             if (index !== dogIndex) return dog;
            
             if (event.target.checked === true) {
@@ -92,17 +95,17 @@ export default class DogForm extends Component<{}, IStateDog> {
         return (
             <div className="flex-form">
                 <form action="">
-                   {this.state.dogs.map((dog:IDogObject, index: number) => {
+                   {this.state.dogs.map((dog:IDog, index: number) => {
                        let dogId = `dog-${index}`, breedId = `breed-${index}`
                        return (
                            <div key={index}>
                                <div className="form-group">
                                     <label htmlFor="dogName">{`Dog #${index + 1} name:`}</label>
-                                    <input type="text" value={dog.dogName} name="dogName" id={dogId} data-id={index} className="form-control dogName" onChange={this.handleNameChange(index)}/>
+                                    <input type="text" value={dog.name} name="dogName" id={dogId} data-id={index} className="form-control dogName" onChange={this.handleNameChange(index)}/>
                                </div>
                                <div className="form-group">
                                     <label htmlFor="dogBreed">Breed: </label>
-                                    <input type="text" value={dog.dogBreed} name="dogBreed" id={breedId} data-id={index} className="form-control dogBreed" onChange={this.handleBreedChange(index)}/>
+                                    <input type="text" value={dog.breed} name="dogBreed" id={breedId} data-id={index} className="form-control dogBreed" onChange={this.handleBreedChange(index)}/>
                                </div>
                                <div className="form-group form-check">
                                    <input type="checkbox" checked={dog.food} name="food" className="form-check-input" onChange={this.handleFoodCheck(index)}/>
