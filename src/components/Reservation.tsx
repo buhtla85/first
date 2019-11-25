2ef7abb04fbd435fa29ab6cc64093472a29b918e
buhtla85 <tamara.naunovic@gmail.com>
2019-11-25 14:57:18 +0100

    function for totalprice added

--- src/components/Reservation.tsx
+++ src/components/Reservation.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import RoomForm from "./RoomForm";
 import { differenceInDays } from "date-fns";
+import DogForm from './DogForm';
 
 
 interface IDog {
@@ -28,6 +29,9 @@ interface IStateRoom {
     rooms: IRoom[],
     ownerName: string,
     ownerEmail: string, 
+    errMessage: string,
+    errMessageName: string,
+    errMessageMail: string,
     totalPrice: number 
 }
 
@@ -38,6 +42,9 @@ export default class Reservation extends Component <{}, IStateRoom> {
                 rooms: [{startDate: "", endDate: "", dogs: [{name: "", breed: "", food: false, grooming: false, foodPrice: 0, groomPrice: 0}], dropDownText: "Chose a room...", days: 0, errMessageDate: "", errMessageDogs: "", counter: 0, roomPrice: 0}], 
                 ownerName: "",
                 ownerEmail: "",
+                errMessage: "",
+                errMessageName: "",
+                errMessageMail: "",
                 totalPrice: 0
         }
     }
@@ -54,7 +61,7 @@ export default class Reservation extends Component <{}, IStateRoom> {
         const newStartDate = this.state.rooms.map((room: IRoom, roomIdx: number) => 
             idx !== roomIdx ? room : {...room, startDate: event.target.value}
         );
-        this.setState({rooms: newStartDate});
+        this.setState({rooms: newStartDate}, () => this.calculateDays(idx)); // this evaluates to NaN first when user inserts start date
     }
 
     addEndDate = (idx: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -147,12 +154,13 @@ export default class Reservation extends Component <{}, IStateRoom> {
         this.setState({rooms: newBreed});
     }
 
-    isFoodChecked = (idx: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    isFoodChecked = (idx: number) => (index: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
         const foodCheck = this.state.rooms.map((room: IRoom, roomIdx: number) => {
             if (idx !== roomIdx) {
                 return room;
             } else {
-                const newValue = room.dogs.map((dog: IDog) => {
+                const newValue = room.dogs.map((dog: IDog, dogIdx: number) => {
+                    if (index !== dogIdx) return dog;
                     if (event.target.checked === true) {
                         return {...dog, food: event.target.checked, foodPrice: 2};
                     } else {
@@ -166,12 +174,13 @@ export default class Reservation extends Component <{}, IStateRoom> {
         this.setState({rooms: foodCheck});
     }
 
-    isGroomingChecked = (idx: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    isGroomingChecked = (idx: number) => (index: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
         const groomCheck = this.state.rooms.map((room: IRoom, roomIdx: number) => {
             if (idx !== roomIdx) {
                 return room;
             } else {
-                const newValue = room.dogs.map((dog: IDog) => {
+                const newValue = room.dogs.map((dog: IDog, dogIdx: number) => {
+                    if (index !== dogIdx) return dog;
                     if (event.target.checked === true) {
                         return {...dog, grooming: event.target.checked, groomPrice: 10};
                     } else {
@@ -185,12 +194,23 @@ export default class Reservation extends Component <{}, IStateRoom> {
         this.setState({rooms: groomCheck});
     }
 
-    addNewDog = (idx: number) => () => { //there are some problems with this function - inconsistencies with code and weird behaviour when user tries to add a dog without selecting type of room first
+    addNewDog = (idx: number) => () => { //there are some problems with this function - code inconsistencies 
         const addDog = this.state.rooms.map((room: IRoom, roomIdx: number) => {
             if (idx !== roomIdx) return room;
+            
             if (room.dogs.length <= room.counter - 1) {
                 const newValue = room.dogs.concat([{name:"", breed: "", food: false, grooming: false, foodPrice: 0, groomPrice: 0}]);
                 const newItem = {...room, dogs: newValue};
+                return newItem;
+            } else if (room.dogs.length <= 0) {
+                const newValue = {...room, errMessageDogs: "Please be sure to chose a room type first."};
+                setTimeout(() => {
+                    const addEmptyString = this.state.rooms.map((room: IRoom, roomIdx: number) => 
+                        idx !== roomIdx ? room : {...room, errMessageDogs: ""}
+                    );
+                    this.setState({rooms: addEmptyString})
+                }, 5000);
+                const newItem = newValue;
                 return newItem;
             } else {
                 const newValue = {...room, errMessageDogs: "Maximum number of dogs for selected room exceeded."};
@@ -206,20 +226,115 @@ export default class Reservation extends Component <{}, IStateRoom> {
         });
         this.setState({rooms: addDog})
     }
+
+    removeDog = (idx: number) => (index: number) => () => {
+        const removeDogs = this.state.rooms.map((room: IRoom, roomIdx: number) => {
+            if (idx !== roomIdx) {
+                return room;
+            } else {
+                const newValue = room.dogs.filter((dog: IDog, dogIdx: number) => index !== dogIdx);
+                const newItem = {...room, dogs: newValue};
+                return newItem;
+            }
+        });
+        this.setState({rooms: removeDogs})
+    }
+
+    addOwnerName = (event: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({ownerName: event.target.value}) 
+    }
+
+    addOwnerEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({ownerEmail: event.target.value}) 
+    }
+
+    handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        this.validateName(this.state.ownerName);
+        this.validateEmail(this.state.ownerEmail);
+        if (this.state.rooms.length <= 0) {
+            this.setState({errMessage: "Please be sure to add a room first."});
+            setTimeout(() => {
+                this.setState({errMessage: ""});
+            }, 5000);
+        }
+
+        const price = this.state.rooms.reduce((acc: number, room: IRoom, currRoom: number, arr: IRoom[]): number => {
+            let val: number = 0;
+
+            if (room.days == 0 || room.roomPrice <= 0) {
+                this.setState({errMessage: "Please be sure to select dates and/or room type."});
+                setTimeout(() => {
+                    this.setState({errMessage: ""});
+                }, 5000);
+            } else {
+                const dogValueGroom = arr[currRoom].dogs.reduce((acc: number, dog: IDog, currDog: number, arrDog: IDog[]): number => {
+                    let zeroValGroom: number = 0;
+                    zeroValGroom = zeroValGroom + acc + dog.groomPrice;
+                    return zeroValGroom;
+                }, 0);
+                const dogValueFood = arr[currRoom].dogs.reduce((acc: number, dog: IDog, currDog: number, arrDog: IDog[]): number => {
+                    let zeroValFood: number = 0;
+                    zeroValFood = zeroValFood + acc + dog.foodPrice;
+                    return zeroValFood;
+                }, 0);
+                val = val + acc + room.days * room.roomPrice + (dogValueGroom + (room.days * dogValueFood));
+                return val;
+            } 
+            return val;
+        }, 0);
+        this.setState({totalPrice: price})
+    }
+
+    validateName = (userName: string) => {
+        if (userName.length < 2) {
+            this.setState({errMessageName: "Invalid input. Name should be at least 2 characters long."});
+            setTimeout(() => {
+                this.setState({errMessageName: ""});
+            }, 4000)
+        }
+    }
+
+    validateEmail = (userEmail: string) => { // it did not work with includes('@') probably due to bootstrap scripts 
+        if (userEmail.length == 0) {
+            this.setState({errMessageMail: "Invalid input. Please enter a valid email."});
+            setTimeout(() => {
+                this.setState({errMessageMail: ""});
+            }, 4000);
+        }
+    }
     
     render() {
         return (
             <div className="container pt-3">
-                <form action="">
+                <form action="" onSubmit={this.handleSubmit}>
                     {this.state.rooms.map((room: IRoom, idx: number) => {
                         return (
                             <div key={idx}>
-                                <RoomForm singleRoom={room} index={idx} changeStartDate={this.addStartDate(idx)} changeEndDate={this.addEndDate(idx)} changeSelected={this.handleDropdown(idx)} handleDogName={this.addDogName(idx)} handleDogBreed={this.addBreedName(idx)} handleFood={this.isFoodChecked(idx)} handleGrooming={this.isGroomingChecked(idx)} handleAddingDogs={this.addNewDog(idx)}/>
+                                <RoomForm singleRoom={room} index={idx} changeStartDate={this.addStartDate(idx)} changeEndDate={this.addEndDate(idx)} changeSelected={this.handleDropdown(idx)} handleDogName={this.addDogName(idx)} handleDogBreed={this.addBreedName(idx)} handleFood={this.isFoodChecked(idx)} handleGrooming={this.isGroomingChecked(idx)} handleAddingDogs={this.addNewDog(idx)} handleRemovingDogs={this.removeDog(idx)}/>
                                 <button type="button" className="btn btn-outline-danger" onClick={this.removeRoom(idx)}>Remove Room</button>
                             </div>
                         )
                     })}
                     <button type="button" className="btn btn-outline-success mt-1" onClick={this.addRoom}>Add New Room</button>
+                    <div className="container pt-2">
+                        <div className="form-group">
+                            <label htmlFor="ownerName">Owner's Name: </label>
+                            <input type="text" name="ownerName" onChange={this.addOwnerName} value={this.state.ownerName} className="form-control"/>
+                            <p className="text-danger">{this.state.errMessageName}</p>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="ownerEmail">Email: </label>
+                            <input type="email" name="ownerEmail" onChange={this.addOwnerEmail} value={this.state.ownerEmail} className="form-control"/>
+                            <p className="text-danger">{this.state.errMessageMail}</p>
+                        </div>
+                        <p className="text-danger">{this.state.errMessage}</p>
+                        <div className="d-flex justify-content-center">
+                            <button type="submit" className="btn btn-success mt-2">Submit</button>
+                        </div>
+                        <p>{`Total price is ${this.state.totalPrice} dollars.`}</p>
+                    </div>
+            
                 </form>
             </div>
         )
