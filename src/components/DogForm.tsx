8c1b3579aa3ac683fb86cfa62c454d20b1a51865
buhtla85 <tamara.naunovic@gmail.com>
2019-12-01 18:26:50 +0100

    minor changes to inputs

--- src/components/DogForm.tsx
+++ src/components/DogForm.tsx
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
 
+export interface IDog {
+    name: string,
+    breed: string,
+    food: boolean,
+    grooming: boolean,
+    foodPrice: number,
+    groomPrice: number
+}
+
 export interface IDogProp {
     singleDog: {name: string, breed: string, food: boolean, grooming: boolean, foodPrice: number, groomPrice: number},
     index: number,
@@ -26,11 +35,11 @@ export default class DogForm extends Component<IDogProp, {}> {
                     <input type="text" name="breed" value={this.props.singleDog.breed} onChange={this.props.changeBreed} className="form-control"/>
                 </div>
                 <div className="form-group form-check">
-                    <input type="checkbox" checked={this.props.singleDog.food} name="food" onChange={this.props.foodCheck} className="form-check-label"/>
+                    <input type="checkbox" checked={this.props.singleDog.food} name="food" id="food" onChange={this.props.foodCheck} className="form-check-label"/>
                     <label htmlFor="food">Our Food (2$ per day)</label>
                 </div>
                 <div className="form-group form-check">
-                    <input type="checkbox" checked={this.props.singleDog.grooming} name="grooming" onChange={this.props.groomCheck} className="form-check-label"/>
+                    <input type="checkbox" checked={this.props.singleDog.grooming} name="grooming" id="grooming" onChange={this.props.groomCheck} className="form-check-label"/>
                     <label htmlFor="grooming">Grooming (10$ per stay)</label>
                 </div>
             </div>
