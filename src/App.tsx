4c464898a2ec701fc8b2b0464d589f0f95af55dc
buhtla85 <tamara.naunovic@gmail.com>
2020-01-13 16:52:18 +0100

    deleted import for error page

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,13 @@ import React, {Fragment} from 'react';
 import './App.css';
 
 //import Home from './pages/Home';
-//import Bookings from './pages/Bookings';
+import Bookings from './pages/Bookings';
 //import Contacts from './pages/Contacts';
-//import ErrorPage from './pages/ErrorPage';
-import DogForm from "./components/DogForm"
-
-//import {Route, Switch} from 'react-router-dom';
 
 const App: React.SFC = () => {
     return (
         <Fragment>
-           <DogForm />
+            <Bookings />
         </Fragment>
     )
 }
