eb8d015437a9b075c279d501d1ea212a63149d5b
buhtla85 <tamara.naunovic@gmail.com>
2020-01-13 16:51:45 +0100

    deleted a tag attribute

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
-//import {FaBars} from 'react-icons/fa';
 
 interface INavState {
     isOpen: boolean;
 }
 
 
-
 export default class Navbar extends React.Component<{}, INavState> {
     constructor(props: any) {
         super(props);
         this.state = {
-            isOpen: false
+            isOpen: false, 
         }
     }
    
@@ -24,9 +22,9 @@ export default class Navbar extends React.Component<{}, INavState> {
             <React.Fragment>
                 <div className={this.state.isOpen ? "topnav responsive" : "topnav"}>
                     <a href="#" ><span className="text-primary">FurBall</span> Hotel</a>
-                    <a href="#" className="active">Home</a>
-                    <a href="#">Bookings</a>
-                    <a href="#">Contact</a>
+                    <a href="#" >Home</a>
+                    <a href="#" >Bookings</a>
+                    <a href="#" >Contact</a>
                     <button type="button" className="icon" onClick={this.handleToggle}>
                         <i className='fa fa-bars'></i>
                     </button>
